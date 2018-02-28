ae697af82fd27ceb427e7a0b4a96e8bc4da2f7d8
Robin Usi <usirobin@gmail.com>
2018-02-27 19:26:00 -0800

    First commit

---
+++ viewtemplate/js/main.js
@@ -0,0 +1,32 @@
+
+  var num = 10;
+var num2 = 22;
+var str = "word";
+var str = "10";
+
+ //console.log(num + num2)
+ //console.log(str)
+ //console.log(str2)
+
+ //var arr = [5, 17, 28, 19, 66];
+ //console.log(arr[3] +arr [1])
+
+ //var personOject = {
+ //		firstName: "Millie",
+ //		lastName: "Usi",
+ //		eyeColor: "brown",
+ //		petType: "dog"
+
+ //}
+
+ //console.log(personOject.petType + " " + personOject.firstName)
+var num = 10;
+var num1 = 19
+
+if (num === "10") {
+console.log('true')
+} else  if (num == 10) {
+console.log ("they're equal")
+}  else {
+	console.log ('false')
+}
